f5d663aa1b6682e433f140563dbb147260ccd11a
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-27 04:39:01 +0300

    Added a dom content loaded event listener

--- main.js
+++ main.js
@@ -4,7 +4,26 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
-
+//Added a dom content loaded event listener
+document.addEventListener("DOMContentLoaded", function() {
+  let l = document.querySelectorAll(".like-glyph");
+  let modal = document.querySelector("#modal")
+  // looped through all like icons and added an event listner for each
+  l.forEach(i=>{
+    i.addEventListener("click", e=>{
+      // handled the server request and changed the icons based on response
+      mimicServerCall().then(r=>{i.classList.contains("activated-heart")?(i.innerHTML = EMPTY_HEART,i.classList.remove("activated-heart")):(i.innerHTML = FULL_HEART,i.classList.add("activated-heart"))}).catch((e)=>{
+        document.querySelector("#modal-message").textContent = e;
+        modal.classList.remove("hidden");
+        setTimeout(() => {
+          //reset the modal
+          modal.classList.add("hidden");
+        document.querySelector("#modal-message").textContent = "";
+        }, 2000);
+      })
+    })
+  })
+});
 
 
 //------------------------------------------------------------------------------
